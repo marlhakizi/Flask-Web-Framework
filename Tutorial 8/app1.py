3bbafc2bed4d332ce370a586ccd879b8f9a14c64
EC2 Default User <ec2-user@ip-172-31-5-142.ec2.internal>
2022-04-04 05:05:23 +0000

    all

--- Tutorial 8/app1.py
+++ Tutorial 8/app1.py
@@ -4,33 +4,33 @@ from facedetection import getimage
 from werkzeug.utils import secure_filename
 import os
 import cv2
-app = Flask(__name__)
 
+#PEOPLE_FOLDER = os.path.join('static', 'people_photo') 
+#app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
+#app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 @app.route('/')
 
 def my_form():
     return render_template('my-form.html')
 
 
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['POST',"GET"])
 # def look_up():
 #     # results = get_comments(videoid)
 #     channelid=request.form.get("channelid")
 #     results = get_channelinfo(channelid)
 #     return jsonify(results)
-    
+  
 def look_up():
     # results = get_comments(videoid)
     file=request.files['file']
     name=secure_filename(file.filename)
     file.save(name)
     image = getimage([name])
-    cv2.imwrite('annotated_image.png', image)
-    #img_url = url_for('static', filename='annotated_image.png')
-    #print(img_url)
-    okay=cv2.imread('annotated_image.png')
-    #cv2.imwrite('annotated_image' + str(idx) + '.png', annotated_image)
-    return render_template('results.html', new="okay")
+    cv2.imwrite('static/annotated_image.png',image)
+    img_url = url_for('static', filename='annotated_image.png')
+    return render_template('results.html', new=img_url)
 
 
 if __name__ == '__main__':
